720aed136aed1970bbb1bf2e1832c0616e756016
yubinkarki <iamyubinkarki@gmail.com>
2024-12-11 21:32:51 +0545

    Values & types

---
+++ you_dont_know_js/up_and_going/chapter-01/03-values-and-types.js
@@ -0,0 +1,22 @@
+/* VALUE & TYPES */
+
+// different representation of for values are called types in programming terminology
+// JS has built-in types for each of the primitive values
+// values that are included directly into the source code are called literals: "okay", 42, true
+// beyond string/number/boolean value types, it's common for programming languages to provide arrays, objects, functions
+
+/* CONVERTING BETWEEN TYPES */
+
+// conversion of one type to another, like number to string or vice versa is called coercion
+
+// explicitly converting to a number
+var fiftyTwo = "52";
+var fiftyTwoNum = Number(fiftyTwo);
+console.log(fiftyTwo);
+console.log(fiftyTwoNum);
+
+// if you use == loose equals operator here, JS will convert the left hand side to it's number equivalent
+console.log("99.99" == 99.99);
+
+// while designed to help you, implicit conversion can create confusion if you haven't taken the time to learn the rules
+// implicit coercion is a mechanism that should be learned by anyone wishing to take JS programming seriously
