99b6fdf04f2b908c4ff5b9ebd5119ab11159f459
yubinkarki <iamyubinkarki@gmail.com>
2024-12-28 17:17:37 +0545

    Book two chapter one

---
+++ you_dont_know_js/02_types_and_grammar/chapter-01/02-built-in-types.ts
@@ -0,0 +1,64 @@
+// @ts-nocheck
+
+/* BUILT-IN TYPES */
+
+// JS defines seven built-int types:
+//  null
+//  undefined
+//  string
+//  number
+//  boolean
+//  object
+//  symbol - added in ES6
+
+// all of these types except object are called primitives
+
+// the 'typeof' operator inspects the type of the given value, and always returns one of seven string values
+// surprisingly, there’s not an exact 1-to-1 match with the seven built-in types we just listed:
+
+typeof undefined === "undefined"; // true
+typeof true === "boolean"; // true
+typeof 42 === "number"; // true
+typeof "42" === "string"; //true
+typeof { life: 42 } === "object"; // true
+typeof Symbol() === "symbol"; // true
+
+// as you may have noticed, I excluded null from the above listing
+// it’s special—special in the sense that it’s buggy when combined with the typeof operator:
+
+typeof null === "object"; // true
+
+// it would have been nice (and correct!) if it returned "null", but this original bug in JS has persisted for nearly two decades
+// and will likely never be fixed because there’s so much existing web content that relies on its buggy behavior that “fixing” the bug would create more “bugs” and break a lot of web software
+
+// if you want to test for a null value using its type, you need a compound condition:
+
+var a = null;
+!a && typeof a === "object"; // true
+
+// null is the only primitive value that is “falsy” (aka false-like; see Chapter 4) but which also returns "object" from the typeof check
+
+typeof function a() {} === "function"; // true
+
+// it’s easy to think that function would be a top-level built-in type in JS, especially given this behavior of the typeof operator
+// however, if you read the spec, you’ll see it’s actually somewhat of a “subtype” of object
+// specifically, a function is referred to as a “callable object” — an object that has an internal [[Call]] property that allows it to be invoked
+
+// the fact that functions are actually objects is quite useful
+// most importantly, they can have properties - for example:
+
+function sum(numOne, numTwo) {}
+
+// the function object has a length property set to the number of formal parameters it is declared with
+
+console.log("Num of properties in sun function", sum.length); // 2
+
+// what about arrays?
+// they’re native to JS, so are they a special type?
+
+typeof [1, 2, 3] === "object"; // true
+
+// nope, just objects
+// it’s most appropriate to think of them also as a “subtype” of object (see Chapter 3)
+// in this case with the additional characteristics of being numerically indexed (as opposed to just being string-keyed like plain objects)
+// and maintaining an automatically updated length property
---
+++ you_dont_know_js/02_types_and_grammar/chapter-01/03-values-as-types.ts
@@ -0,0 +1,132 @@
+// @ts-nocheck
+
+/* VALUES AS TYPES */
+
+// In JS, variables don’t have types — values have types
+// variables can hold any value, at any time
+// if you use typeof against a variable, it’s not asking “What’s the type of the variable?” as it may seem
+// since JS variables have no types - instead, it’s asking “What’s the type of the value in the variable?”
+
+var a = 42;
+typeof a; // "number"
+
+a = true;
+typeof a; // "boolean"
+
+// the typeof operator always returns a string
+typeof typeof 42; // "string"
+
+// the first typeof 42 returns "number", and typeof "number" is "string"
+
+/* UNDEFINED VS UNDECLARED */
+
+// variables that have no value currently actually have the undefined value
+// calling typeof against such variables will return "undefined":
+
+var name;
+console.log("Type of name", typeof name); // undefined
+
+var age = 50;
+var gender;
+
+age = gender;
+console.log("Type of age", typeof age); // undefined
+console.log("Type of gender", typeof gender); // undefined
+
+// it’s tempting for most developers to think of the word “undefined” as a synonym for “undeclared"
+// however, in JS, these two concepts are quite different
+// an “undefined” variable is one that has been declared in the accessible scope, but at the moment has no other value in it
+// by contrast, an “undeclared” variable is one that has not been formally declared in the accessible scope
+
+console.log("Accessing undeclared variable 'location'", location); // ReferenceError: location is not defined
+
+//an annoying confusion is the error message that browsers assign to this condition
+// as you can see, the message is “location is not defined”, which is of course very easy and reasonable to confuse with “location is undefined”
+// yet again, “undefined” and “is not defined” are very different things
+// it’d be nice if the browsers said something like “location is not found” or “location is not declared” to reduce the confusion!
+
+// there’s also a special behavior associated with typeof as it relates to undeclared variables that even further reinforces the confusion
+
+var x;
+console.log("Type of x", typeof x); // "undefined"
+console.log("Type of y", typeof y); // "undefined"
+
+// the typeof operator returns "undefined" even for “undeclared” (or “not defined”) variables
+// notice that there was no error thrown when we executed typeof y, even though y is an undeclared variable
+// this is a special safety guard in the behavior of typeof
+// similar to above, it would have been nice if typeof used with an undeclared variable returned “undeclared” instead of conflating the result value with the different “undefined” case
+
+/* TYPEOF UNDECLARED */
+
+// nevertheless, this safety guard is a useful feature when dealing with JS in the browser
+// where multiple script files can load variables into the shared global namespace
+
+// s a simple example, imagine having a “debug mode” in your program that is controlled by a global variable (flag) called DEBUG
+// you’d want to check if that variable was declared before performing a debug task like logging a message to the console
+// a top-level global var DEBUG = true declaration would only be included in a “debug.js” file, which you only load into the browser when you’re in development/testing, but not in production
+
+// however, you have to take care in how you check for the global DEBUG variable in the rest of your application code so that you don’t throw a ReferenceError
+// the safety guard on typeof is our friend in this case:
+
+// oops, this would throw an error!
+// if (DEBUG) {
+//   console.log("Debugging");
+// }
+
+if (typeof DEBUG !== "undefined") {
+  console.log("Debugging");
+}
+
+// this sort of check is useful even if you’re not dealing with user-defined variables (like DEBUG)
+// if you are doing a feature check for a built-in API, you may also find it helpful to check without throwing an error:
+
+if (typeof atob === "undefined") {
+  atob = function () {};
+}
+
+// another way of doing these checks against global variables but without the safety guard feature of typeof is to observe that all global variables are also properties of the global object, which in the browser is basically the window object
+// so, the above checks could have been done (quite safely) as:
+
+// if (window.DEBUG) {
+// }
+
+// if (window.atob) {
+// }
+
+// unlike referencing undeclared variables, there is no ReferenceError thrown if you try to access an object property (even on the global window object) that doesn’t exist
+// on the other hand, manually referencing the global variable with a window reference is something some developers prefer to avoid, especially if your code needs to run in multiple JS environments (not just browsers, but server-side node.js, for instance), where the global variable may not always be called window
+// technically, this safety guard on typeof is useful even if you’re not using global variables, though these circumstances are less common, and some developers may find this design approach less desirable
+// imagine a utility function that you want others to copy-and-paste into their programs or modules, in which you want to check to see if the including program has defined a certain variable (so that you can use it) or not:
+
+function doSomethingCool() {
+  var helper = typeof FeatureXYZ !== "undefined" ? FeatureXYZ : function () {};
+  var val = helper();
+}
+
+// doSomethingCool() tests for a variable called FeatureXYZ, and if found, uses it, but if not, uses its own
+// now, if someone includes this utility into their module/program, it safely checks if they’ve defined FeatureXYZ or not:
+
+(function () {
+  function FeatureXYZ() {}
+
+  function doSomethingCool() {
+    var helper = typeof FeatureXYZ !== "undefined" ? FeatureXYZ : function () {};
+    var val = helper();
+  }
+
+  doSomethingCool();
+})();
+
+// here, FeatureXYZ is not at all a global variable, but we’re still using the safety guard of typeof to make it safe to check for
+// and importantly, here there is no object we can use (like we did for global variables with window.___) to make the check, so typeof is quite helpful
+
+// other developers would prefer a design pattern called “dependency injection” where instead of doSomethingCool() inspecting implicitly for FeatureXYZ to be defined outside/around it, it would need to have the dependency explicitly passed in, like:
+
+function doAnotherThing(FeatureABC) {
+  var helper = FeatureABC || function () {};
+  var val = helper();
+}
+
+// there’s lots of options when designing such functionality
+// no one pattern here is “correct” or “wrong” — there are various trade-offs to each approach
+// but overall, it’s nice that the typeof undeclared safety guard gives us more options
