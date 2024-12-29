feccfc5551d27959d5648c693e163b0e2dc0a9b3
yubinkarki <iamyubinkarki@gmail.com>
2024-12-29 16:20:37 +0545

    Chapter two values

---
+++ you_dont_know_js/02_types_and_grammar/chapter-02/01-values.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck
+
+/* VALUES */
+
+// arrays, strings, and numbers are the most basic building blocks of any program, but JS has some unique characteristics with these types that may either delight or confound you
+// let’s look at several of the built-in value types in JS, and explore how we can more fully understand and correctly leverage their behaviors
