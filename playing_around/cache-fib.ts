5315bd750e4dc485edc9f4f594a81b0307b68d9d
yubinkarki <iamyubinkarki@gmail.com>
2025-02-22 18:57:46 +0545

    Add scope in file

--- playing_around/cache-fib.ts
+++ playing_around/cache-fib.ts
@@ -1,59 +1,58 @@
-// 0 1 1 2 3 5 8 13 21
+{
+  // 0 1 1 2 3 5 8 13 21
 
-class CacheFibClass {
-  /**
-   * Cache for storing calculated Fibonacci numbers.
-   */
-  private fibCache: Map<number, number> = new Map();
+  class CacheFibClass {
+    private fibCache: Map<number, number> = new Map();
 
-  /**
-   * Recursively calculates the Fibonacci number for the given position.
-   * @param num - The position in the Fibonacci sequence.
-   * @returns The Fibonacci number at the given position.
-   */
-  public calculate(num: number): number {
-    if (num === 2) {
-      this.fibCache.set(num, 1);
-      return 1;
+    /**
+     * Recursively calculates the Fibonacci number for the given position.
+     * @param num - The position in the Fibonacci sequence.
+     * @returns The Fibonacci number at the given position.
+     */
+    public calculate(num: number): number {
+      if (num === 2) {
+        this.fibCache.set(num, 1);
+        return 1;
+      }
+
+      if (num === 1) {
+        this.fibCache.set(num, 0);
+        return 0;
+      }
+
+      console.log(">>", num);
+
+      if (this.fibCache.has(num)) return this.fibCache.get(num)!;
+
+      const result = this.calculate(num - 1) + this.calculate(num - 2);
+
+      this.fibCache.set(num, result);
+
+      return result;
     }
 
-    if (num === 1) {
-      this.fibCache.set(num, 0);
-      return 0;
+    /**
+     * Returns a string representation of the Fibonacci sequence, sorted in ascending order, using the cached values.
+     * @returns A comma-separated string of Fibonacci numbers with spaces after the commas.
+     */
+    public showSequence(): string {
+      const cacheValues: MapIterator<number> = this.fibCache.values();
+      const cacheValueList: number[] = Array.from(cacheValues).sort((a, b) => a - b);
+
+      if (cacheValueList.length > 10) {
+        const firstHalf: string = cacheValueList.slice(0, 8).join(", ");
+        const secondHalf: string = cacheValueList.slice(-2).join(", ");
+        return firstHalf + " . . . " + secondHalf;
+      } else return cacheValueList.join(", ");
     }
-
-    console.log(">>", num);
-
-    if (this.fibCache.has(num)) return this.fibCache.get(num)!;
-
-    const result = this.calculate(num - 1) + this.calculate(num - 2);
-
-    this.fibCache.set(num, result);
-
-    return result;
   }
 
-  /**
-   * Returns a string representation of the Fibonacci sequence, sorted in ascending order, using the cached values.
-   * @returns A comma-separated string of Fibonacci numbers with spaces after the commas.
-   */
-  public showSequence(): string {
-    const cacheValues: MapIterator<number> = this.fibCache.values();
-    const cacheValueList: number[] = Array.from(cacheValues).sort((a, b) => a - b);
+  const fib: CacheFibClass = new CacheFibClass();
+  const numberToCalculate: number = 45;
 
-    if (cacheValueList.length > 10) {
-      const firstHalf: string = cacheValueList.slice(0, 8).join(", ");
-      const secondHalf: string = cacheValueList.slice(-2).join(", ");
-      return firstHalf + " . . . " + secondHalf;
-    } else return cacheValueList.join(", ");
-  }
+  console.time("⏳ fib calculation time");
+  const fibResult: number = fib.calculate(numberToCalculate);
+  console.timeEnd("⏳ fib calculation time");
+  console.log(`Fib result in sequence ${numberToCalculate} =`, fibResult);
+  console.log(`Fib sequence till ${numberToCalculate} sequence =`, fib.showSequence());
 }
-
-const fib: CacheFibClass = new CacheFibClass();
-const numberToCalculate: number = 45;
-
-console.time("⏳ fib calculation time");
-const fibResult: number = fib.calculate(numberToCalculate);
-console.timeEnd("⏳ fib calculation time");
-console.log(`Fib result in sequence ${numberToCalculate} =`, fibResult);
-console.log(`Fib sequence till ${numberToCalculate} sequence =`, fib.showSequence());
--- playing_around/plain-fib.ts
+++ playing_around/plain-fib.ts
@@ -1,26 +1,28 @@
-// 0 1 1 2 3 5 8 13 21
+{
+  // 0 1 1 2 3 5 8 13 21
 
-class PlainFibClass {
-  /**
-   * Recursively calculates the Fibonacci number for the given position.
-   * @param num - The position in the Fibonacci sequence.
-   * @returns The Fibonacci number at the given position.
-   */
-  public calculate(num: number): number {
-    if (num === 2) return 1;
+  class PlainFibClass {
+    /**
+     * Recursively calculates the Fibonacci number for the given position.
+     * @param num - The position in the Fibonacci sequence.
+     * @returns The Fibonacci number at the given position.
+     */
+    public calculate(num: number): number {
+      if (num === 2) return 1;
 
-    if (num === 1) return 0;
+      if (num === 1) return 0;
 
-    console.log(">>", num);
+      console.log(">>", num);
 
-    return this.calculate(num - 1) + this.calculate(num - 2);
+      return this.calculate(num - 1) + this.calculate(num - 2);
+    }
   }
+
+  const plainFib: PlainFibClass = new PlainFibClass();
+  const numberToCal: number = 30;
+
+  console.time("⌛️ fib calculation time");
+  const plainFibResult = plainFib.calculate(numberToCal);
+  console.timeEnd("⌛️ fib calculation time");
+  console.log(`Fib result in sequence ${numberToCal} =`, plainFibResult);
 }
-
-const plainFib: PlainFibClass = new PlainFibClass();
-const numberToCal: number = 30;
-
-console.time("⌛️ fib calculation time");
-const plainFibResult = plainFib.calculate(numberToCal);
-console.timeEnd("⌛️ fib calculation time");
-console.log(`Fib result in sequence ${numberToCal} =`, plainFibResult);
