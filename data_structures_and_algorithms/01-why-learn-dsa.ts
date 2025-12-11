fdf8e2228425a125f5332713c51ced06228e19eb
yubinkarki <iamyubinkarki@gmail.com>
2025-12-11 21:52:21 +0900

    なぜ DSA 学ぶの？

---
+++ data_structures_and_algorithms/01-why-learn-dsa.ts
@@ -0,0 +1,35 @@
+// @ts-nocheck
+
+/* WHY LEARN DATA STRUCTURES & ALGORITHMS */
+
+// the study of data structures is one of efficiency
+// given a particular amount of data
+// what is the best way to store it to achieve a particular goal?
+// as a programmer, you regularly use a variety of collection types
+// such as lists maps and sets
+// these are data structures that hold a collection of data
+// each structure having its own performance characteristics
+
+// for example, consider the difference between a list and a set
+// both are meant to hold a collection of elements
+// but searching for an element in a list takes far longer than searching for an element in a set
+// on the other hand, you can order the elements of a list but you can’t order the elements of a set
+
+// data structures are a well-studied discipline, and the concepts are language agnostic
+// a data structure from C is functionally and conceptually identical
+// to the same data structure in any other language, such as Dart
+// at the same time, the high-level expressiveness of Dart makes it an ideal choice
+// for learning these core concepts without sacrificing too much performance
+
+// algorithms, on the other hand, are a set of operations that complete a task
+// this can be a sorting algorithm that moves data around to put it in order
+// or it can be an algorithm that compresses an 8K picture to a manageable size
+// algorithms are essential to software
+// and many have been created to act as building blocks for useful programs
+
+// using an appropriate data structure is crucial when working with lots of data
+// and using the right algorithm plays a significant role
+// in the performance and scalability of your software
+// your mobile apps will be more responsive and have better battery life
+// your server apps will be able to handle more concurrent requests and use less energy
+// algorithms often include proofs of correctness that you can leverage to build better software
