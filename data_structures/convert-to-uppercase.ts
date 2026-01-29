f02f718117aade4c4b89804e1eef33e61564f8fe
yubinkarki <iamyubinkarki@gmail.com>
2026-01-29 22:28:48 +0900

    大文字に変換する

---
+++ data_structures/convert-to-uppercase.ts
@@ -0,0 +1,36 @@
+/* 
+Take a string and convert all characters to uppercase
+Replace space by * except for any leading and trailing spaces
+Those spaces should be left as it is
+
+Example: (underscore means whitespace)
+
+Input 1: Hello_my_friend 
+Output 1: HELLO*MY*FRIEND  
+
+Input 2: __No_Way__
+Output 2: __NO*WAY__
+*/
+{
+  function toUpper(myInput: string): string {
+    const upper: string = myInput.toUpperCase();
+    const chars: string[] = upper.split("");
+
+    let start: number = 0;
+    let end: number = chars.length - 1;
+
+    while (start < chars.length && chars[start] === " ") start++;
+    while (end >= 0 && chars[end] === " ") end--;
+
+    console.log(start, end);
+
+    for (i = start; i <= end; i++) {
+      console.log(chars[i]);
+      if (chars[i] === " ") chars[i] = "*";
+    }
+    
+    return chars.join("");
+  }
+
+  console.log(toUpper(" Hello my friend "))
+}
