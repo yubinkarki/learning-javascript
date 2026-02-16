583533436ace37370e5801768bed550f17c8043c
yubinkarki <iamyubinkarki@gmail.com>
2026-02-16 21:52:38 +0900

    preface を移行する

---
+++ 本/you_dont_know_js/preface.ts
@@ -0,0 +1,40 @@
+// @ts-nocheck
+
+/* PREFACE */
+
+// JS has been a foundational technology that drives interactive experience around the content we consume
+// few doubt its importance at the heart of the world's most widely available software platform: the Web
+// but as a language
+// it has perpetually been a target for a great deal of criticism due to its design philosophy
+// while JS is perhaps one of the easiest languages to get up nd running with
+// its eccentricities make solid mastery of the language
+// a vastly less common occurrence than in many other languages
+// sophisticated concepts that are deeply rooted into the language
+// tend instead to surface themselves in seemingly simplistic ways
+// such as passing around functions as callbacks
+// which encourages the JS developer to just the use the language as is
+// and not worry too much about what's going on under the hood
+// because JS can be used without understanding, the understanding of the language is often never attained
+// the JS you know right now is probably
+// parts handed down to you by others who've been burned by incomplete understanding
+// that JS is but a shadow of the true language
+// you don't really know JS yet, but if you dig into this series, you will
+
+// i’m sure you noticed, but “JS” in the series title
+// is not an abbreviation for words used to curse about JS
+// though cursing at the language’s quirks is something we can probably all identify with!
+// from the earliest days of the Web
+// JS has been a foundational technology that drives interactive experience around the content we consume
+// while flickering mouse trails and annoying pop-up prompts may be where JS started
+// nearly two decades later, the technology and capability of JS has grown many orders of magnitude
+// and few doubt its importance at the heart of the world’s most widely available software platform: the Web
+// but as a language, it has perpetually been a target for a great deal of criticism
+// owing partly to its heritage but even more to its design philosophy
+// even the name evokes, as Brendan Eich once put it, “dumb kid brother”
+// status next to its more mature older brother Java
+// but the name is merely an accident of politics and marketing
+// the two languages are vastly different in many important ways
+// “JavaScript” is as related to “Java” as “Carnival” is to “Car”
+// it is simultaneously a simple, easy-to-use language that has broad appeal
+// and a complex and nuanced collection of language mechanics
+// that without careful study will elude true understanding even for the most seasoned of JS developers
